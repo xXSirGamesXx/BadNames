903c99327ed95193945912d6644de0bd72c27b38
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-07-31 23:20:36 -0400

    Update and rename xXSirGamesXx/BadNames/Main.php to src/xXSirGamesXx/BadNames/Main.php

--- src/xXSirGamesXx/BadNames/Main.php
+++ src/xXSirGamesXx/BadNames/Main.php
@@ -14,12 +14,12 @@ class Main extends PluginBase{
 
 		$this->saveDefaultConfig();
         $this->reloadConfig();		
-		$this->getLogger()->info("Hello World!");
+		$this->getLogger()->info("Em=nabling BadNames by xXSirGamesXx");
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 		$this->c = new Config($this->getDataFolder() . "names.txt", Config::ENUM);
 	}
 
 	public function onDisable(){
-		$this->getLogger()->info("Bye");
+		$this->getLogger()->info("Disabling BadNames by xxsirbutterxx ");
 	}
 }
